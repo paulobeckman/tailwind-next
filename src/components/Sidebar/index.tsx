be2f0b9b7877aa27ccbe6cf7d4a11f6bfed1106c
paulobeckman <beckmam.paulo@hotmail.com>
2023-09-22 15:28:37 -0300

    feat: pattern de composição

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,19 +1,22 @@
-import { BarChart, CheckSquare, ChevronDown, Flag, Home, SquareStack, Users, Search, LifeBuoy, Cog } from "lucide-react";
+import { BarChart, CheckSquare, Flag, Home, SquareStack, Users, LifeBuoy, Cog, Search } from "lucide-react";
 
 import { Logo } from "./Logo";
 import { NavItem } from "./NavItem";
 import { UsedSpaceWidget } from "./UsedSpaceWidget";
 import { Profile } from "./Profile";
+import * as Input from "@/components/Input"
 
 export function Sidebar() {
   return (
     <aside className='border-r border-zinc-200 px-5 py-8 flex flex-col gap-6'>
       <Logo />
 
-      <div className="flex mx-1 w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className='w-5 h-5 text-zinc-500' />
-        <input className='flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600' placeholder='Search' />
-      </div>
+      <Input.Root>
+        <Input.Prefix>
+          <Search className='w-5 h-5 text-zinc-500' />
+        </Input.Prefix>
+        <Input.Control placeholder="Search"/>
+      </Input.Root>
 
       <nav className="space-y-0.5">
         <NavItem title="Home" icon={Home} />
