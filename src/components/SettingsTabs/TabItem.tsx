83c53fa0a9448f2c374b402d777b37917c410ef3
paulobeckman <beckmam.paulo@hotmail.com>
2023-09-25 14:53:32 -0300

    feat: Criando Abas com Radix TABS

---
+++ src/components/SettingsTabs/TabItem.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import * as Tabs from '@radix-ui/react-tabs';
+
+export interface TabItemProps {
+  value: string
+  title: string
+  isSelected?: boolean
+}
+
+export function TabItem({ value, title, isSelected = false }: TabItemProps) {
+    return (
+      <Tabs.Trigger value={value} className='relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 date-[state=active]:text-violet-500'>
+      <span>{title}</span>
+
+      {isSelected && (
+        <div className='absolute left-0 right-0 h-0.5 bg-violet-700 -bottom-px'></div>
+      )}
+    </Tabs.Trigger>
+    )
+}
